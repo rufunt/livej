bf45ffae23b912300362087b3723c12a63070466
rufunt <jazzoo@mail.ru>
2018-09-25 20:22:19 +0000

    Add Edit and Update actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,36 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+  end
+  
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :summary, :body)
+  end
 end
