d032319c9cfcff5f953f594d080bb2e0cbcf7ffc
rufunt <jazzoo@mail.ru>
2018-09-30 11:35:37 +0000

    Tuning Editor Trix

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :posts
+  resources :pictures, only: [:create, :destroy]
 
   # Example resource route with options:
   #   resources :products do
