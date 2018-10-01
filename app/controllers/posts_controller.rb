232ddde79723d218537b2ea214ba51708395d3fb
rufunt <jazzoo@mail.ru>
2018-10-01 13:32:21 +0000

    Update form with tags and localization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:title, :summary, :body, :image)
+    params.require(:post).permit(:title, :summary, :body, :image, :all_tags)
   end
   
   def find_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
-  has_many :tags, through: :tagings
+  has_many :tags, through: :taggings
   has_many :taggings
   validates :title, :summary, :body, presence: true
+  
+  def all_tags
+    self.tags.map(&:name).join(', ')
+  end
+  
+  def all_tags=(names)
+    self.tags = names.split(',').map do |name|
+      Tag.where(name: name.strip).first_or_create
+    end
+  end
+  
 end
