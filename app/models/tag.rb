2006023f49f5fc35ca38347acc2eeb874fb459a2
rufunt <jazzoo@mail.ru>
2018-10-01 14:00:32 +0000

    Add View and Controller for Model Tag and action Show

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ActiveRecord::Base
-  has_many :posts, through: :tagings
+  has_many :posts, through: :taggings
   has_many :taggings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :posts
   resources :pictures, only: [:create, :destroy]
+  resources :tags, only: [:show]
 
   # Example resource route with options:
   #   resources :products do
