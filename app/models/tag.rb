24636581d6d332c65e446fec0a460bb6d634d23e
rufunt <jazzoo@mail.ru>
2018-10-01 14:42:30 +0000

    Add Tags Cloud

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ActiveRecord::Base
   has_many :posts, through: :taggings
   has_many :taggings
+  
+  def self.counts
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("name, taggings.tag_id")
+  end
 end
