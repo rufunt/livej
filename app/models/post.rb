ad4bcf17c299fabda77c274279cb7811613bff30
rufunt <jazzoo@mail.ru>
2018-10-01 12:13:09 +0000

    Add Tag and Tagging Models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  has_many :tags, through: :tagings
+  has_many :taggings
   validates :title, :summary, :body, presence: true
 end
